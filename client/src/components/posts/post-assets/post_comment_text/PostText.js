9f361e844f45b9ab461ea3f61ccd64a05dbf5eb8
borgatajunky <mad1083@gmail.com>
2019-01-21 18:48:01 -0500

    added reactStringReplace to handle urls, hashtags, and mentions without using dangerouslySetInnerHTML

--- client/src/components/posts/post-assets/post_comment_text/PostText.js
+++ client/src/components/posts/post-assets/post_comment_text/PostText.js
@@ -1,16 +1,42 @@
-import React, {Fragment} from 'react'
+import React, { Component } from 'react'
+import reactStringReplace from 'react-string-replace'
+import { Link } from 'react-router-dom'
 
-export default function PostText({ postText }) {
-  return (
-    <Fragment>
-      <p className='post_content' >
-        { <span className='hashtags' style={{color: 'rgb(205,205,205)'}}
-            dangerouslySetInnerHTML={{
-              __html : postText.replace(/((\#|@)\w+)/gi, "<a href='#'>$&</a>")
-            }}
-          /> 
-        }
-      </p>
-    </Fragment>
-  )
+
+
+export default class PostText extends Component {
+  render() {
+    let replacedText;
+ 
+    // Match URLs
+    replacedText = reactStringReplace(this.props.postText, /(https?:\/\/\S+)/g, (match, i) => (
+      <a key={match + i} href={match}>{match}</a>
+    ));
+     
+    // Match @-mentions
+    replacedText = reactStringReplace(replacedText, /@(\w+)/g, (match, i) => (
+      <Link key={match + i} to={`/profile/${match}`}>@{match}</Link>
+    ));
+     
+    // Match hashtags
+    replacedText = reactStringReplace(replacedText, /#(\w+)/g, (match, i) => (
+      <a key={match + i} target='_blank' href={`https://twitter.com/hashtag/${match}`}>#{match}</a>
+    ));
+      
+    
+  
+    return (
+      <div className='post_content' >
+        { replacedText }
+      </div>
+    )
+  }
 }
+
+
+
+// <span className='hashtags' style={{color: 'rgb(205,205,205)'}}
+//   dangerouslySetInnerHTML={{
+//     __html : postText.replace(/((\#\w+)|(@)\w+)/gi, "<a href='#'>$&</a>")
+//   }}
+// /> 
