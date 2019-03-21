353baddddfa32fe8e198b5d6192162c4c6fd10f2
borgatajunky <mad1083@gmail.com>
2019-03-21 18:49:36 -0400

    added post info to notifications.js and styled it a bit

--- client/src/components/posts/post-assets/notifications/Notifications.js
+++ client/src/components/posts/post-assets/notifications/Notifications.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux' 
+import './Notifications.css'
 
 class Notifications extends Component {
   render() {
     return (
       <div>
         { this.props.profile.profile && 
-        <div style={{ color: 'rgb(55, 131, 194)', textAlign: 'center' }}>
-          { this.props.profile.profile.notifications.map((val, i) => 
-            <div style={{padding: 10}} key={val._id}>
-              <p><span style={{color:'#999'}}>{val.message}</span></p>
-            </div>).reverse()
-          }
-        </div>
+          <div style={{ color: 'rgb(55, 131, 194)', marginTop: 100 }}>
+            { this.props.profile.profile.notifications.map(notification => 
+              <div className='notifications_container' key={notification._id}>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                  { notification.avatar && <img src={notification.avatar} style={{ width: 30, height: 30, borderRadius: '50%', marginRight: 10 }} />}
+                  { notification.message && <p><span style={{color:'#999'}}>{notification.message}</span></p> }
+                </div>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                  { notification.postText && <p>{ notification.postText.length >= 47 ? notification.postText.slice(0, 50) + '...' : notification.postText }</p> }
+                  { notification.postImage && <img src={notification.postImage} style={{ width: 30, height: 30, marginLeft: 10 }} />}
+                </div>
+              </div>
+            ).reverse()}
+          </div>
         }
       </div>
     )
