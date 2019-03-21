fd9a17ec33ea78cf1a164bbd5dc837d0d1665652
borgatajunky <mad1083@gmail.com>
2019-03-20 23:18:36 -0400

    changed postlikes popupmenu from onHover to onClick and onMouseLeave

--- client/src/components/posts/post-assets/notifications/Notifications.js
+++ client/src/components/posts/post-assets/notifications/Notifications.js
@@ -9,8 +9,8 @@ class Notifications extends Component {
         <div style={{ color: 'rgb(55, 131, 194)', textAlign: 'center' }}>
           { this.props.profile.profile.notifications.map((val, i) => 
             <div style={{padding: 10}} key={val._id}>
-              <p>{i+1}. <span style={{color:'#999'}}>{val.message}</span></p>
-            </div>)
+              <p><span style={{color:'#999'}}>{val.message}</span></p>
+            </div>).reverse()
           }
         </div>
         }
