3af3620ddbafe66f34a47767ecbaa6e51f4fa73f
borgatajunky <mad1083@gmail.com>
2019-04-09 23:31:23 -0400

    background color test on mobile

--- client/src/store.js
+++ client/src/store.js
@@ -8,7 +8,7 @@ const middleware = [thunk]
 
 const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware),
   // window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose
-  // window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
 export default store
