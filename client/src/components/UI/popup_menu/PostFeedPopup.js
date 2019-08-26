16e88456159d0fe141a2c377ab1b38a88a7ab931
marlonanthony <mad1083@gmail.com>
2019-08-25 23:48:00 -0400

    edited media queries on profile page and added box-sizing: border-box

--- client/src/components/UI/popup_menu/PostFeedPopup.js
+++ client/src/components/UI/popup_menu/PostFeedPopup.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react'
 class PostFeedPopup extends Component {
 
   render() {
-    const { profile: {profiles}, popupHandler, post, showPopup, userNameOrAvatarClicked  } = this.props
+    const { profile: { profiles }, popupHandler, post, showPopup, userNameOrAvatarClicked  } = this.props
     let profileInfo = profiles && profiles.map(profile =>  {
       if(profile.user._id === post.user) {
         return (
           <div key={profile.user._id}>
-            <p style={{ color: '#aaa', fontSize: 13, padding: '0px 5px', cursor: 'text' }}>{profile.bio}</p> 
+            <p style={{ color: '#aaa', fontSize: 13, padding: '0px 5px', cursor: 'text' }}>{ profile.bio }</p> 
             <a style={{textDecoration: 'none'}} href={profile.website} target='_blank'>
-              <p style={{ color: 'rgb(29, 138, 228)', fontSize: 13 }}>{profile.website}</p>
+              <p style={{ color: 'rgb(29, 138, 228)', fontSize: 13 }}>{ profile.website }</p>
             </a>
           </div>
         )
