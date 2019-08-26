be9ba78061e50a519099941a9b9c84dced4ee059
marlonanthony <mad1083@gmail.com>
2019-08-26 18:09:39 -0400

    removed serviceWorker and App.test.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 // import axios from 'axios' 
 // window.axios = axios 
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
