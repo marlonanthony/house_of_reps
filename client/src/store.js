d98bfbfd5360989b37c4a6e660ab0d25929d672e
marlonanthony <mad1083@gmail.com>
2019-08-26 01:49:23 -0400

    removed README.md from client folder and testing out redux middleware conditional for prod

--- client/src/store.js
+++ client/src/store.js
@@ -7,7 +7,9 @@ const initialState = {}
 const middleware = [thunk]
 
 const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware),
-  // window.__REDUX_DEVTOOLS_EXTENSION__() && window.__REDUX_DEVTOOLS_EXTENSION__()
+  process.env.NODE_ENV === 'production' 
+    ? window.__REDUX_DEVTOOLS_EXTENSION__() && window.__REDUX_DEVTOOLS_EXTENSION__()
+    : ''
 ))
 
 export default store
