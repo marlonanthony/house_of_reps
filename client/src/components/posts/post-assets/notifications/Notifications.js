0e56bbd12096d65df10ef8f03b8bbea9d0c3b5d6
marlonanthony <mad1083@gmail.com>
2019-08-26 22:08:24 -0400

    refactored Notifications/ShowPost.js and created NotificationList.js

--- client/src/components/posts/post-assets/notifications/Notifications.js
+++ client/src/components/posts/post-assets/notifications/Notifications.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
 import { connect } from 'react-redux' 
-import Moment from 'react-moment' 
 import axios from 'axios'
 
 import { getPost } from '../../../../actions/postActions'
 import ShowPost from './ShowPost'
+import NotificationList from './NotificationList'
 import './Notifications.css'
 
 class Notifications extends Component {
@@ -16,7 +15,6 @@ class Notifications extends Component {
   }
 
   componentDidMount() {
-    document.addEventListener('click', this.onOutsideClick, true) 
     axios.get('/api/profile/notifications') 
     .then(res => {
       this.setState({ notifications: res.data })
@@ -32,15 +30,7 @@ class Notifications extends Component {
     this.setState(prevState => ({ showPost: !prevState.showPost }))
   }
 
-  onOutsideClick = (e) => {
-    const domNode = ReactDOM.findDOMNode(this) 
-    if(!domNode || !domNode.contains(e.target)) {
-      this.setState({ showPost: false })
-    }
-  }
-
   componentWillUnmount() {
-    document.removeEventListener('click', this.onOutsideClick, true)
     axios.post('/api/profile/notifications/seen')
   }
 
@@ -64,51 +54,12 @@ class Notifications extends Component {
           />
         }
 
-        { this.state.notifications && 
-          <div className='notifications'>
-            <h1 id='notifications_header'>Notifications</h1>
-            { this.state.notifications.map(notification => (
-              <div className={!notification.seen ? 'new_notification' : 'notifications_container' } key={notification._id}>
-                <div className='notification_avatar_and_message_container'>
-                  { notification.avatar && <img src={notification.avatar} alt='avatar' className='notification_user_avatar' />}
-                  { notification.message && <p><span className='notification_message'>{notification.message}</span></p> }
-                </div>
-                <div className='notification_post_content' onClick={this.postHandler.bind(this, notification.postId)}>
-                  { notification.postText && <p>{ notification.postText.length >= 47 ? notification.postText.slice(0, 50) + '...' : notification.postText }</p> }
-                  { notification.postImage && <img src={notification.postImage} alt='post img' className='notification_post_content_image' />}
-                  { notification.highlight && <iframe title='youtube' className='notification_highlights' src={notification.highlight.video} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe> }
-                  { !notification.highlight && notification.video && <iframe title='youtube' className='notification_highlights' src={notification.video} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe> }
-                </div>
-                <div className='notification_icons'>
-                  { notification.message && notification.message.includes('liked') 
-                    ? <i className='fas fa-thumbs-up icons'></i>
-                    : notification.message && notification.message.includes('commented') 
-                    ? <i className='fas fa-comment icons' id='comment'/>                     
-                    : null
-                  }
-                  { notification.highlight && <p style={{fontSize: 12}}>{ notification.highlight.likes && notification.highlight.likes.length }</p> }
-                  { notification.post && <p style={{fontSize: 12}}>{ notification.post.likes && notification.post.likes.length }</p> }
-                  { notification.comment && <p style={{fontSize: 12}}>{ notification.comment.likes && notification.comment.likes.length }</p> }
-                  { notification.date && (
-                    <p className='notification_date'>{ notification.date && Math.abs(new Date(notification.date) - new Date()) > 259200000 
-                      ?  <Moment format='ddd, ll LT'>{notification.date}</Moment>
-                      : Math.abs(new Date(notification.date) - new Date()) > 172800000 
-                      ? '2 days ago'
-                      : Math.abs(new Date(notification.date) - new Date()) >= 86400000
-                      ? 'yesterday'
-                      : Math.abs(new Date(notification.date) - new Date()) > 7200000
-                      ? Math.floor(Math.abs(new Date(notification.date) - new Date()) / 3600000) + ' hours ago'
-                      : Math.abs(new Date(notification.date) - new Date()) > 3600000
-                      ? '1 hour ago'
-                      : Math.abs(new Date(notification.date) - new Date()) >= 60000
-                      ? Math.floor(Math.abs(new Date(notification.date) - new Date()) / 60000) + ' minutes ago'
-                      : Math.floor(Math.abs(new Date(notification.date) - new Date()) / 1000) + ' seconds ago'
-                    }</p>
-                  )}
-                </div>
-              </div>
-            ))}
-          </div>
+        { this.state.notifications &&
+          <NotificationList 
+            notifications={this.state.notifications} 
+            postHandler={this.postHandler}
+            modalToggle={this.modalToggle}
+          />
         }
       </div>
     )
@@ -120,10 +71,3 @@ const mapStateToProps = state => ({
 })
 
 export default connect(mapStateToProps, { getPost  })(Notifications)
-
-    //   31, 449, 600, 000 ms === 1 Year
-    //    2, 592, 000, 000 ms === 1 Month (30 Days)
-    //       604, 800, 000 ms === 1 Week
-    //        86, 400, 000 ms === 1 Day
-    //         3, 600, 000 ms === 1 Hour
-    //             60, 000 ms === 1 minute 
--- client/src/components/posts/post-assets/notifications/ShowPost.js
+++ client/src/components/posts/post-assets/notifications/ShowPost.js
@@ -1,39 +1,61 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import HighlightsModal from '../../../UI/modal/highlights-modal/HighlightsModal';
 import PostText from '../post_comment_text/PostText'
 import Backdrop from '../../../UI/backdrop/Backdrop'
 
-const ShowPost = ({ post, modalToggle, showPost, youtubeUrl }) => (
-  <div className='notifications_modal_wrapper'>
-    <Backdrop clicked={modalToggle} show={showPost} />
-    <HighlightsModal>
-      { !post.description && !post.image && !post.title && !post.url && !post.media
-        ? <PostText postText={post.text && post.text} />
-        : post.media 
-        ? ( <div>
-              <PostText postText={post.text && post.text} />
-              <img className='postfeed-media-pic' onClick={modalToggle} src={post.media && post.media} alt="uploaded" />
-            </div>
-          )
-        : ( <div className='post_content'>
-              <PostText postText={post.text && post.text} />
-              <div style={{ background: 'rgba(0, 0, 0, .5)', borderRadius: '5px' }}>
-                { youtubeUrl && youtubeUrl
-                ? <div style={{ display: 'flex', justifyContent: 'center', margin: '0 auto' }}>
-                    <iframe title='youtube' width="100%" height="300" src={youtubeUrl && youtubeUrl} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe> 
+class ShowPost extends React.Component {
+  
+  componentDidMount() {
+    document.addEventListener('click', this.onOutsideClick, true)
+  }
+
+  onOutsideClick = (e) => {
+    const { modalToggle } = this.props
+    const domNode = ReactDOM.findDOMNode(this) 
+    if(!domNode || !domNode.contains(e.target)) modalToggle()
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.onOutsideClick, true)
+  }
+
+  render () {
+    const { post, modalToggle, showPost, youtubeUrl } = this.props
+
+    return (
+      <div className='notifications_modal_wrapper'>
+        <Backdrop clicked={modalToggle} show={showPost} />
+        <HighlightsModal>
+          { !post.description && !post.image && !post.title && !post.url && !post.media
+            ? <PostText postText={post.text && post.text} />
+            : post.media 
+            ? ( <div>
+                  <PostText postText={post.text && post.text} />
+                  <img className='postfeed-media-pic' onClick={modalToggle} src={post.media && post.media} alt="uploaded" />
+                </div>
+              )
+            : ( <div className='post_content'>
+                  <PostText postText={post.text && post.text} />
+                  <div style={{ background: 'rgba(0, 0, 0, .5)', borderRadius: '5px' }}>
+                    { youtubeUrl && youtubeUrl
+                    ? <div style={{ display: 'flex', justifyContent: 'center', margin: '0 auto' }}>
+                        <iframe title='youtube' width="100%" height="300" src={youtubeUrl && youtubeUrl} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe> 
+                      </div>
+                    : <a href={post.url && post.url} target='_blank' rel='noopener noreferrer'>
+                        <img src={post.image && post.image} alt='thumbnail' style={{ width: '100%' }} id='post-link-img' />
+                      </a> 
+                    }
+                    <p style={{textAlign: 'center', fontSize: '12px'}}>{post.title && post.title}</p>
+                    <p style={{textAlign: 'center', fontSize: '12px', padding: '0 5px 20px 5px'}}>{post.description && post.description}</p>
                   </div>
-                : <a href={post.url && post.url} target='_blank' rel='noopener noreferrer'>
-                    <img src={post.image && post.image} alt='thumbnail' style={{ width: '100%' }} id='post-link-img' />
-                  </a> 
-                }
-                <p style={{textAlign: 'center', fontSize: '12px'}}>{post.title && post.title}</p>
-                <p style={{textAlign: 'center', fontSize: '12px', padding: '0 5px 20px 5px'}}>{post.description && post.description}</p>
-              </div>
-            </div>
-          )
-      }
-    </HighlightsModal>
-  </div>
-)
+                </div>
+              )
+          }
+        </HighlightsModal>
+      </div>
+    )
+  }
+}
 
 export default ShowPost
