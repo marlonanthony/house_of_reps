e442b510b2fd8f6c294b4d8378778b60e6b961b3
marlonanthony <mad1083@gmail.com>
2019-08-27 00:09:22 -0400

    Refactored ProfileCreds.js and created ModalVenues.js

--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -1,10 +1,10 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { likeVenue } from '../../actions/profileActions'
-// import Moment from 'react-moment' 
 import HighlightsModal from '../UI/modal/highlights-modal/HighlightsModal'
 import Backdrop from '../UI/backdrop/Backdrop'
-
+import VenueItems from './profile_assets/VenueItems'
+import ModalVenues from './profile_assets/ModalVenues'
 import './ProfileCreds.css'
 
 class ProfileCreds extends Component {
@@ -25,52 +25,26 @@ class ProfileCreds extends Component {
     const highlightsModal = this.state.showModal && (
       <Fragment>
         <HighlightsModal>
-          { venues.map(venue => (
-            <div key={venue._id} style={{ marginBottom: 40, border: '0.03px solid rgba(0, 255, 255, 0.03)' }}>
-              { venue.title && <p style={{color: 'rgb(29, 138, 255)'}}>{venue.title}</p> }
-              { venue.video && 
-                <iframe
-                  title={venue.video}
-                  src={venue.video}
-                  frameBorder={0}
-                  allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-                  allowFullScreen={true}
-                  style={{ width: '95%', height: '80vh', marginTop: '15px' }}>
-                </iframe> 
-              }
-              { !venue.video && venue.image && 
-                <img src={venue.image} style={{maxWidth: '100%', maxHeight: '100%'}} alt='profile_hightlights' />
-              }
-              { venue.description && <p style={{ color: '#666', padding: '0px 10px' }}>{venue.description}</p> }
-              <div style={{display: 'flex', alignItems: 'center', paddingLeft: 10}}>
-                <i onClick={() => this.likeHighlight(venue._id, venue.user)} className='fas fa-thumbs-up icons' style={{color: 'cyan', cursor: 'pointer'}}></i>
-                <p style={{color: 'cyan'}}>{venue.likes && venue.likes.length}</p>
-              </div>
-            </div>
-          ))
-          }
+          <ModalVenues
+            venues={venues}
+            likeHighlight={this.likeHighlight}
+          />
         </HighlightsModal>
       </Fragment>
     ) 
 
     const venueItems = venues.map(venue => (
-      <li key={venue._id} className='venue-items'>
-        { venue.video &&
-          <iframe id='profile_creds_video' title={venue._id}  src={venue.video} width="330" height="200" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe>
-        }
-        { venue.image && !venue.video &&
-          <div style={{display: 'flex', justifyContent: 'center' }}>
-            <img src={venue.image} id='venue_profile_image' alt="new-venue"/>
-          </div>
-        }
-        <img onClick={this.modalToggle} className='profile_highlights_toggle_modal' src={require('../../img/hor-icon.jpg')} alt='hors' title={'🔥'}/>
-      </li>
+      <VenueItems 
+        key={venue._id}
+        venue={venue}
+        modalToggle={this.modalToggle}
+      />
     ))
+
     return (
       <div className='profile-creds' style={{marginTop: 70}}>
         <Backdrop clicked={this.modalToggle} show={this.state.showModal} />
-        {highlightsModal}
-        <h4 id='profile-events-h4'>Media</h4>
+        { highlightsModal }
         <div className='profile-creds-content'>
           { venueItems.length > 0 ? ( 
             venueItems.filter((val, i) => i < 4 ?  (<ul>{venueItems}</ul>) : null)
