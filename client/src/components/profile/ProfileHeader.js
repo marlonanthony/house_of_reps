c854d9c7bc4abc66ede22cbb31d1fc6d0401708a
marlonanthony <mad1083@gmail.com>
2019-08-26 23:36:46 -0400

    turned ProfileHeader.js into a functional component

--- client/src/components/profile/ProfileHeader.js
+++ client/src/components/profile/ProfileHeader.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import isEmpty from '../../validation/is-empty'
 import ProfileCreds from './ProfileCreds'
 import ProfilePost from './ProfilePost'
@@ -7,30 +7,25 @@ import Banners from './profile_assets/Banners'
 import ProfileAbout from './profile_assets/ProfileAbout'
 import './ProfileHeader.css'
 
-class ProfileHeader extends Component {
-  render() {
-    const { profile, user } = this.props 
-    return (
-      <div className='profile-header-container'>
-        <div className="profile-header">
-          <div id='header-items'>
-            <div id='profile-avatar-div'>
-              {isEmpty(user.avatar) ? <p>create profile</p> : <img id='profile-avatar' src={profile.user.avatar} alt="avatar" />}
-            </div>
-            <ProfileAbout profile={profile} />
-            <ProfileIcons profile={profile} />
-            <Banners />
+export default function ProfileHeader({ profile, user }) {
+  return (
+    <div className='profile-header-container'>
+      <div className="profile-header">
+        <div id='header-items'>
+          <div id='profile-avatar-div'>
+            {isEmpty(user.avatar) ? <p>create profile</p> : <img id='profile-avatar' src={profile.user.avatar} alt="avatar" />}
           </div>
-          <div id='profile-feed'>
-            <ProfilePost />
-          </div>
-          <div id='profile-creds-div'>
-            <ProfileCreds venues={profile.venues} />
-          </div>
+          <ProfileAbout profile={profile} />
+          <ProfileIcons profile={profile} />
+          <Banners />
+        </div>
+        <div id='profile-feed'>
+          <ProfilePost />
+        </div>
+        <div id='profile-creds-div'>
+          <ProfileCreds venues={profile.venues} />
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
-
-export default ProfileHeader
