e19eb4dde91d54d61f8979a09d0a1d921703a1e5
marlonanthony <mad1083@gmail.com>
2019-08-28 16:57:59 -0400

    moved avatar and name into profileinfo variable in PostFeedPopup.js and changed styling

--- client/src/components/UI/popup_menu/PostFeedPopup.js
+++ client/src/components/UI/popup_menu/PostFeedPopup.js
@@ -4,32 +4,30 @@ export default function PostFeedPopup(props) {
   const { profile: { profiles }, popupHandler, post, showPopup, userNameOrAvatarClicked  } = props
   
   const profileInfo = profiles && profiles.map(profile => (
-    profile.user._id === post.user ? 
-      <div key={profile.user._id}>
-        <p style={{ color: '#aaa', fontSize: 13, padding: '0px 5px', cursor: 'text' }}>{ profile.bio }</p> 
-        <a style={{textDecoration: 'none'}} href={profile.website} target='_blank' rel='noopener noreferrer'>
-          <p style={{ color: 'rgb(29, 138, 228)', fontSize: 13 }}>{ profile.website }</p>
-        </a>
-      </div> : null
-  ))
-
-  return (
-    <div className='popup' onMouseOver={popupHandler} onMouseOut={popupHandler}>
-      <p className='post_name' 
-        onClick={() => this.props.userNameOrAvatarClicked(post.user)}>
-        { post.name }
-      </p>
-      <div className={showPopup ? 'show popupcontent' : 'popupcontent'}>
+    profile.user._id === post.user && 
+      <div className='postfeed_popup_profile_info' key={profile.user._id}>
         <img 
           onClick={() => userNameOrAvatarClicked(post.user)} 
           className='popup-profile-img' 
           src={ post.avatar } 
           alt={ post.name } />
-        <span
-          onClick={() => userNameOrAvatarClicked(post.user)}
-          style={{color: 'rgb(29, 138, 228)', fontSize: 13}}>
+        <span onClick={() => userNameOrAvatarClicked(post.user)}>
           { post.name }
         </span>
+        <small>{ profile.bio }</small> 
+        <a href={profile.website} target='_blank' rel='noopener noreferrer'>
+          <p>{ profile.website }</p>
+        </a>
+      </div>
+  ))
+
+  return (
+    <div className='popup' onMouseOver={popupHandler} onMouseOut={popupHandler}>
+      <p className='post_name' 
+        onClick={() => props.userNameOrAvatarClicked(post.user)}>
+        { post.name }
+      </p>
+      <div className={showPopup ? 'show popupcontent' : 'popupcontent'}>
         { profileInfo && profileInfo }
       </div>
     </div>
