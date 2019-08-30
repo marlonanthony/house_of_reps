3662b46ef00b42ef827859f45040c67af60b2ab8
marlonanthony <mad1083@gmail.com>
2019-08-30 00:26:25 -0400

    refactored users.js to async/await

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,96 +4,88 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken') 
 const keys = require('../../config/keys')
 const passport = require('passport')
+
 const Mailer = require('../../services/Mailer')
 const updateTemplate = require('../../services/email_templates/updateTemplate')
-
-// Load Input Validation
 const validateRegisterInput = require('../../validation/register')
 const validateLoginInput = require('../../validation/login')
-
-// Load User Model
 const User = require('../../models/User')
 const Token = require('../../models/Token')
 
-router.post('/confirm', (req, res) => {
-  Token.findOne({ token: req.body.token }).then(response => {
-    User.findById(response._userId)
-    .then(user => {
-      if(user) {
-        user.isVerified = true 
-        user.save()
-        res.json({
-          _id: user.id, // might use id, email, and handle. not sure yet. else get rid of them
-          email: user.email,
-          handle: user.handle,
-          isVerified: user.isVerified
-        })
-      }
+// @route     POST api/users/confirm
+// @desc      Confirm email address
+// @access    Public?
+router.post('/confirm', async (req, res) => {
+  try {
+    const response = await Token.findOne({ token: req.body.token })
+    const user = await User.findById(response._userId)
+    if(!user) throw new Error('User not found')
+    user.isVerified = true
+    await user.save()
+    return res.json({
+      _id: user.id, // might use id, email, and handle. not sure yet. else get rid of them
+      email: user.email,
+      handle: user.handle,
+      isVerified: user.isVerified
     })
-    .catch(err => console.log(err))
-  })
+  } catch(err) { throw err }
 })
 
 // @route         POST api/users/register
 // @description   Register a user
 // @access        Public 
-router.post('/register', (req, res) => {
-  const { errors, isValid } = validateRegisterInput(req.body) 
-  const { name, handle, email, password } = req.body
-  // Check Validation
-  if(!isValid) {
-    return res.status(400).json(errors) 
-  }
+router.post('/register', async (req, res) => {
+  try {
+    const { errors, isValid } = validateRegisterInput(req.body) 
+    const { name, handle, email, password } = req.body
 
-  User.findOne({ email })
-  .then(user => {
+    if(!isValid) {
+      return res.status(400).json(errors) 
+    }
+
+    const user = await User.findOne({ email })
+
     if(user) {
       errors.email = 'Email already exists'
       return res.status(400).json(errors) 
-    } else {
-      const avatar = gravatar.url(email, {
-        s: '200',   // Size
-        r: 'pg',   // rating 
-        d: 'mm'   // default
-      })
-      const newUser = new User({
-        name,
-        handle,
-        email,
-        avatar: req.body.avatar ? req.body.avatar : avatar,
-        password
-      })
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if(err) throw err 
-          newUser.password = hash 
-          newUser.save()
+    } 
 
-          .then(user => { 
-            let num = Math.floor((Math.random() * 10000000000) + 1)
-            const token = new Token({
-              _userId: user._id,
-              token: num
-            })
-              token.save() 
-            // const { recipients } = req.body 
-            const emailInfo = {
-              subject: 'Testing the register route',
-              body: "Bruh, I'm fittin to write all the emails!",
-              recipients: 'mad1083@yahoo.com', // change this to email after testing
-              token: token.token
-            }
-            
-            const mailer = new Mailer(emailInfo, updateTemplate(emailInfo))
-            mailer.send() 
-            res.json(user)
-          })
-          .catch(err => console.log(err)) 
+    const avatar = gravatar.url(email, {
+      s: '200',   // Size
+      r: 'pg',   // rating 
+      d: 'mm'   // default
+    })
+    const newUser = new User({
+      name,
+      handle,
+      email,
+      avatar: req.body.avatar ? req.body.avatar : avatar,
+      password
+    })
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, async (err, hash) => {
+        if(err) throw err 
+        newUser.password = hash 
+        const savedUser = await newUser.save()
+        let num = Math.floor((Math.random() * 10000000000) + 1)
+        const token = new Token({
+          _userId: savedUser._id,
+          token: num
         })
+        await token.save() 
+        const emailInfo = {
+          subject: 'Testing the register route',
+          body: "Bruh, I'm fittin to write all the emails!",
+          recipients: 'mad1083@yahoo.com', // change this to email after testing
+          token: token.token
+        }
+        const mailer = new Mailer(emailInfo, updateTemplate(emailInfo))
+        await mailer.send() 
+        return res.json(user)
       })
-    }
-  })
-  .catch(err => console.log(err)) 
+    })
+  } catch(err) { throw err }
 })
 
 
@@ -126,44 +118,33 @@ router.post('/update/:id', (req, res) => {
 // @route         POST api/users/login
 // @description   Login User / Returning JWT Token
 // @access        Public 
-router.post('/login', (req, res) => {
-  const { errors, isValid } = validateLoginInput(req.body) 
-
-  // Check Validation
-  if(!isValid) {
-    return res.status(400).json(errors) 
-  }
-
-  const email = req.body.email
-  const password = req.body.password 
-
-  // Find user by email
-  User.findOne({ email })
-  .then(user => {
-    // Check for user
+router.post('/login', async (req, res) => {
+  try {
+    const { errors, isValid } = validateLoginInput(req.body) 
+    if(!isValid) return res.status(400).json(errors) 
+    const email = req.body.email
+    const password = req.body.password 
+    const user = await User.findOne({ email })
     if(!user) {
       errors.email = 'User not found'
       return res.status(404).json(errors)
     }
-    // Check Password
-    bcrypt.compare(password, user.password)
-    .then(isMatch => {
-      if(isMatch) {
-        // User Matched 
-        const payload = { id: user.id, name: user.name, avatar: user.avatar, handle: user.handle }
-        // Sign Token
-        jwt.sign(payload, keys.secretOrKey, { expiresIn: 604800 }, (err, token) => {
-          res.json({
-            success: true,
-            token: 'Bearer ' + token 
-          })
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(isMatch) {
+      // User Matched 
+      const payload = { id: user.id, name: user.name, avatar: user.avatar, handle: user.handle }
+      // Sign Token
+      jwt.sign(payload, keys.secretOrKey, { expiresIn: 604800 }, (err, token) => {
+        res.json({
+          success: true,
+          token: 'Bearer ' + token 
         })
-      } else {
-        errors.password = 'Password incorrect'
-        return res.status(400).json(errors)
-      }
-    })
-  })
+      })
+    } else {
+      errors.password = 'Password incorrect'
+      return res.status(400).json(errors)
+    }
+  } catch(err) { throw err }
 })
 
 // @route         GET api/users/current
