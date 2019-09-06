d49dd615ccf9eb92d45645c7ad75d5e37beb7b1b
marlonanthony <mad1083@gmail.com>
2019-09-06 01:39:31 -0400

    edited styling on likes popup

--- client/src/components/posts/post-assets/post_item_assets/PostItemLikes.js
+++ client/src/components/posts/post-assets/post_item_assets/PostItemLikes.js
@@ -14,15 +14,15 @@ const PostItemLikes = ({
       : <div  onClick={likesPopupHandler} className='popup_likes'> Liked by {likes.map(like => <span key={like.user}>{ like.name }</span>)}</div>
     }
     <div onMouseLeave={likesPopupHandler} className={ showLikesPopup ? 'show likespopupcontent' : 'likespopupcontent'}>
-      <div style={{ position: 'absolute', top: 5, left: 5 }}>
+      <div>
         <i className='fas fa-thumbs-up icons likespopupicon'></i>
         <small>{likes.length}</small>
       </div>
       <div>
-        {likes.length < 1 ? null : likes.map(like => (
+        { likes.length < 1 ? null : likes.map(like => (
           <div className='likespopupavatarandname' key={like.user}>
             <img onClick={() => userNameOrAvatarClickedLikesPopup(like.handle)} className='popup_likes_avatar' alt='avatar' src={like.avatar} />
-            <p onClick={() => userNameOrAvatarClickedLikesPopup(like.handle)} style={{padding: 10 }}>{like.name}</p>
+            <p onClick={() => userNameOrAvatarClickedLikesPopup(like.handle)}>{like.name}</p>
           </div>
         ))}
       </div>
