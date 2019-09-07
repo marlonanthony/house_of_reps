40db27c09676e09999189574d10d9b1a5dc25abc
marlonanthony <mad1083@gmail.com>
2019-09-07 03:53:57 -0400

    refactored NestedCommentNameAvatarDate.js

--- client/src/components/posts/nested_comments/nested_comment_assets/name_avatar_date/NestedCommentNameAvatarDate.js
+++ client/src/components/posts/nested_comments/nested_comment_assets/name_avatar_date/NestedCommentNameAvatarDate.js
@@ -6,10 +6,12 @@ import './NestedCommentNameAvatarDate.css'
 const NestedCommentNameAvatarDate = ({ nestedComment }) => nestedComment && (
   <div className='nested_comment_name_avatar_date_container'>
     <img className='nested_comment_avatar' src={nestedComment.avatar} alt="user avatar"/>
-    <p className='nested_comment_name'>{nestedComment.name}</p>
-    <p className='nested_comment_date'>
-      <Moment format='ddd, ll LT'>{ nestedComment.date }</Moment>
-    </p>
+    <div>
+      <p className='nested_comment_name'>{nestedComment.name}</p>
+      <p className='nested_comment_date'>
+        <Moment format='ddd, ll LT'>{ nestedComment.date }</Moment>
+      </p>
+    </div>
   </div>
 )
 
