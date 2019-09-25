dbdd012ed9a18e5156d1e59bf94b01da0418c643
marlonanthony <mad1083@gmail.com>
2019-09-24 22:02:52 -0400

    created DefaultPostBody.js

---
+++ client/src/components/posts/post-assets/post_item_assets/post_item_body/DefaultPostBody.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PostText from '../../text/PostText'
+
+export default function DefaultPostBody({
+  post,
+  modalToggle,
+  youtubeUrl
+}) {
+  return !post.description && !post.image && !post.title && !post.url && !post.media
+    ? <PostText postText={post.text} />
+    : post.media 
+      ? <div>
+          <PostText postText={post.text} />
+          <img className='postfeed-media-pic' onClick={modalToggle} src={post.media} alt="uploaded" />
+        </div>
+      : <div className='post_content'>
+          <PostText postText={post.text} />
+          <div>
+            { youtubeUrl 
+              ? <iframe 
+                  title='youtube' 
+                  width="100%" 
+                  height="300" 
+                  src={youtubeUrl} 
+                  frameBorder={0} 
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                  allowFullScreen={true}>
+                </iframe>
+              : <a href={post.url} target='_blank' rel='noopener noreferrer'>
+                  <img src={post.image} alt='thumbnail' style={{ width: '100%' }} id='post-link-img' />
+                </a> 
+            }
+            <p style={{textAlign: 'center', fontSize: '12px'}}>{post.title}</p>
+            <p style={{textAlign: 'center', fontSize: '12px', padding: '0 5px 20px 5px'}}>{post.description}</p>
+          </div>
+        </div>
+}
--- client/src/components/posts/post-assets/post_item_assets/post_item_body/PostBody.js
+++ client/src/components/posts/post-assets/post_item_assets/post_item_body/PostBody.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import JSEMOJI from 'emoji-js'
 
-import PostText from '../text/PostText'
-import { editPostAction } from '../../../../actions/postActions'
+import { editPostAction } from '../../../../../actions/postActions'
 import EditPostBody from './EditPostBody'
+import DefaultPostBody from './DefaultPostBody'
 
 class PostBody extends Component {
   state = {
@@ -65,38 +65,13 @@ class PostBody extends Component {
                              .replace(/&feature=www\.youtube\.com/gi, '')
       : youtubeUrl = null 
 
-    if(!editPost) {
-      return !post.description && !post.image && !post.title && !post.url && !post.media
-        ? <PostText postText={post.text} />
-        : post.media 
-          ? <div>
-              <PostText postText={post.text} />
-              <img className='postfeed-media-pic' onClick={modalToggle} src={post.media} alt="uploaded" />
-            </div>
-          : <div className='post_content'>
-              <PostText postText={post.text} />
-              <div>
-                { youtubeUrl 
-                  ? <iframe 
-                      title='youtube' 
-                      width="100%" 
-                      height="300" 
-                      src={youtubeUrl} 
-                      frameBorder={0} 
-                      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-                      allowFullScreen={true}>
-                    </iframe>
-                  : <a href={post.url} target='_blank' rel='noopener noreferrer'>
-                      <img src={post.image} alt='thumbnail' style={{ width: '100%' }} id='post-link-img' />
-                    </a> 
-                }
-                <p style={{textAlign: 'center', fontSize: '12px'}}>{post.title}</p>
-                <p style={{textAlign: 'center', fontSize: '12px', padding: '0 5px 20px 5px'}}>{post.description}</p>
-              </div>
-            </div>
-    } else {
-      return (
-        <EditPostBody 
+    return !editPost
+      ? <DefaultPostBody
+          post={post}
+          modalToggle={modalToggle}
+          youtubeUrl={youtubeUrl}
+        />
+      : <EditPostBody 
           showEmojis={showEmojis}
           toggleEmoji={this.toggleEmoji}
           addEmoji={this.addEmoji}
@@ -107,8 +82,6 @@ class PostBody extends Component {
           modalToggle={modalToggle}
           youtubeUrl={youtubeUrl}
         />
-      )
-    }
   }
 }
 
