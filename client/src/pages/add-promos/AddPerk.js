d8e543ac4906f4135ffd08f7fab2bd1fe172d085
marlonanthony <mad1083@gmail.com>
2019-09-25 03:13:57 -0400

    refactored AddPerk.js to hooks

--- client/src/pages/add-promos/AddPerk.js
+++ client/src/pages/add-promos/AddPerk.js
@@ -1,54 +1,48 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux' 
 import PropTypes from 'prop-types' 
 import Dropzone from 'react-dropzone' 
 import request from 'superagent'
 
-import { addStore, addPerk } from '../../actions/profileActions'
+import { addPerk } from '../../actions/profileActions'
 import Input from '../../components/common/inputs/Input'
 
 const CLOUDINARY_UPLOAD_PRESET = 'btq6upaq'
 const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/dbwifrjvy/image/upload'
 
-class AddPerk extends Component {
-  state = {
-    image: '',
-    url: '',
-    description: '',
-    errors: {},
-    uploadedFileCloudinaryUrl: '',
-    uploadedFile: ''
-  }
+const AddPerk = ({
+  addPerk,
+  ...props
+}) => {
 
-  componentWillReceiveProps(nextProps) {
-    if(nextProps.errors) {
-      this.setState({ errors: nextProps.errors })
-    }
-  }
+  const [image, setImage] = useState(''),
+  [url, setUrl] = useState(''),
+  [description, setDiscription] = useState(''),
+  [errors, setErrors] = useState({}),
+  [uploadedFileCloudinaryUrl, setUploadedFileCloudinaryUrl] = useState(''),
+  [uploadedFile, setUploadedFile] = useState('')
 
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
+  useEffect(() => {
+    setErrors(props.errors)
+  }, [props.errors])
 
-  onSubmit = e => {
+  const onSubmit = e => {
     e.preventDefault()
-    
     const perkData = {
-      image: this.state.image,
-      url: this.state.url,
-      description: this.state.description
+      image,
+      url,
+      description
     }
-
-    this.props.addPerk(perkData, this.props.history)
+    addPerk(perkData, props.history)
   }
 
-  onImageDrop = files => {
-    this.setState({ uploadedFile: files[0]})
-    this.handleImageUpload(files[0])
+  const onImageDrop = files => {
+    setUploadedFile(files[0])
+    handleImageUpload(files[0])
   }
 
-  handleImageUpload = (file) => {
+  const handleImageUpload = (file) => {
     let upload = request.post(CLOUDINARY_UPLOAD_URL)
                         .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
                         .field('file', file) 
@@ -56,85 +50,81 @@ class AddPerk extends Component {
     upload.end((err, response) => {
       if(err) console.log(err) 
       if(response.body.secure_url !== '') {
-        this.setState({ uploadedFileCloudinaryUrl: response.body.secure_url})
-        this.setState({ image: response.body.secure_url })
+        setUploadedFileCloudinaryUrl(response.body.secure_url)
+        setImage(response.body.secure_url)
       }
     })
   }
 
-  render() {
-    const { errors } = this.state 
-    return (
-      /*  Set classNames to djpools for lack of repitition  */ 
-      <div className='add-djpool'>
-        <i onClick={this.props.history.goBack} id='addvenue-back-button' className='fas fa-arrow-alt-circle-left' alt='back-button' />
-        <h2>Add Perk</h2>
-        <div className='djpools_input_wrapper'>
-          <div className='djpools-dropzone'>
-            <div className='FileUpload'>
-              <Dropzone 
-                className='dropzone'  // In UI/dropzone
-                multiple={false}
-                accept='image/*'
-                onDrop={this.onImageDrop}>
-                <p>Drag or click here to upload a file.</p>
-              </Dropzone>
-            </div>
-            <div>
-              { this.state.uploadedFileCloudinaryUrl === '' ? null : 
-                <img 
-                  src={this.state.uploadedFileCloudinaryUrl} 
-                  style={{ height: '50px', width: '50px' }}
-                  alt={this.state.uploadedFile.name} />
-              }
-            </div>
+  return (
+    /*  Set classNames to djpools for lack of repitition  */ 
+    <div className='add-djpool'>
+      <i 
+        onClick={props.history.goBack} 
+        id='addvenue-back-button' 
+        className='fas fa-arrow-alt-circle-left' 
+        alt='back-button' 
+      />
+      <h2>Add Perk</h2>
+      <div className='djpools_input_wrapper'>
+        <div className='djpools-dropzone'>
+          <div className='FileUpload'>
+            <Dropzone 
+              className='dropzone'  // In UI/dropzone
+              multiple={false}
+              accept='image/*'
+              onDrop={onImageDrop}>
+              <p>Drag or click here to upload a file.</p>
+            </Dropzone>
           </div>
-          <form onSubmit={ this.onSubmit }>
-            <Input 
-              name='url'
-              type='text'
-              value={ this.state.url }
-              onChange={ this.onChange }
-              error={ errors.url }
-              placeholder='URL'
-            />
-            <Input 
-              name='image'
-              type='text'
-              value={ this.state.image }
-              onChange={ this.onChange }
-              error={ errors.image }
-              placeholder='image'
-            />
-            <Input 
-              name='description'
-              type='text'
-              value={ this.state.description }
-              onChange={ this.onChange }
-              error={ errors.description }
-              placeholder='description'
-            />
-            <div style={{textAlign: 'center'}}>
-              <input type="submit" value='Submit' id='add-djpools-submit-button' />
-            </div>
-          </form>
+          <div>
+            { uploadedFileCloudinaryUrl === '' ? null : 
+              <img 
+                src={uploadedFileCloudinaryUrl} 
+                style={{ height: '50px', width: '50px' }}
+                alt={uploadedFile.name} />
+            }
+          </div>
         </div>
+        <form onSubmit={ onSubmit }>
+          <Input 
+            name='url'
+            type='text'
+            value={ url }
+            onChange={e => setUrl(e.target.value) }
+            error={ errors.url }
+            placeholder='URL'
+          />
+          <Input 
+            name='image'
+            type='text'
+            value={ image }
+            onChange={e => setImage(e.target.value) }
+            error={ errors.image }
+            placeholder='image'
+          />
+          <Input 
+            name='description'
+            type='text'
+            value={ description }
+            onChange={e => setDiscription(e.target.value) }
+            error={ errors.description }
+            placeholder='description'
+          />
+          <div style={{textAlign: 'center'}}>
+            <input type="submit" value='Submit' id='add-djpools-submit-button' />
+          </div>
+        </form>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 AddPerk.propTypes = {
-  profile: PropTypes.object.isRequired,
   errors: PropTypes.object.isRequired,
-  addStore: PropTypes.func.isRequired,
   addPerk: PropTypes.func.isRequired
 }
 
-const mapStateToProps = state => ({
-  profile: state.profile,
-  errors: state.errors,
-  auth: state.auth 
-})
+const mapStateToProps = state => ({ errors: state.errors })
 
-export default connect(mapStateToProps, { addStore, addPerk })(withRouter(AddPerk))
+export default connect(mapStateToProps, { addPerk })(withRouter(AddPerk))
