3d82eb25374fb35bb9a969030cc885711803994d
marlonanthony <mad1083@gmail.com>
2019-09-25 00:39:23 -0400

    refactored DropdownMenu.js to hooks

--- client/src/components/common/dropdown_menu/DropdownMenu.js
+++ client/src/components/common/dropdown_menu/DropdownMenu.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import ReactDOM from 'react-dom' 
 import { Link, withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types' 
@@ -7,90 +7,88 @@ import { connect } from 'react-redux'
 import { logoutUser } from '../../../actions/authActions'
 import { getCurrentProfile, clearCurrentProfile } from '../../../actions/profileActions'
 import isEmpty from '../../../validation/is-empty'
-
 import './DropdownMenu.css'
 
-class DropdownMenu extends Component {
+const DropdownMenu = ({
+  logoutUser,
+  clearCurrentProfile,
+  getCurrentProfile,
+  auth,
+  ...props
+}) => {
 
-  state = { displayMenu: false }
+  const [displayMenu, setDisplayMenu] = useState(false),
+        ref = useRef()
 
-  componentDidMount() {
-    if(!this.props.auth.isAuthenticated) this.props.getCurrentProfile() 
-    document.addEventListener('click', this.onOutsideClick, true) 
-  }
+  useEffect(() => {
+    if(!auth.isAuthenticated) getCurrentProfile() 
+    document.addEventListener('click', onOutsideClick, true)
+    return () => {
+      document.removeEventListener('click', onOutsideClick, true) 
+    }
+  }, [])
 
-  componentWillUnmount() {
-    document.removeEventListener('click', this.onOutsideClick, true) 
-  }
-
-  onOutsideClick = (e) => {
-    const domNode = ReactDOM.findDOMNode(this) 
+  const onOutsideClick = e => {
+    const domNode = ReactDOM.findDOMNode(ref.current) 
     if(!domNode || !domNode.contains(e.target)) {
-      this.setState({ displayMenu: false })
+      setDisplayMenu(false)
     }
   }
-  
-  toggleClick = () => {
-    this.setState(prevState => ({ displayMenu: !prevState.displayMenu }))
-  }
 
-  onLogoutClick = e => {
+  const onLogoutClick = e => {
     e.preventDefault()
-    this.props.clearCurrentProfile()
-    this.props.logoutUser() 
+    clearCurrentProfile()
+    logoutUser()
   }
 
-  render() {
-    const { isAuthenticated, user } = this.props.auth 
-    const { profile } = this.props.profile
-    
-    if(isEmpty(profile)) return null
+  const { isAuthenticated, user } = auth 
+  const { profile } = props.profile
   
-    const authLinks = (
-      <>
-        {/* <Link to="/djs">DJs</Link> */}
-        <Link to={`/profile/${profile.handle}`}>Profile</Link>
-        <Link to="/feed">Feed</Link>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="#" onClick={this.onLogoutClick}>
-          <div style={{display: 'flex', alignItems: 'center'}}>
-            <img 
-              src={user.avatar}
-              alt={user.name} 
-              style={{ width: '25px', height: '22px', marginRight: '5px' }}
-            />
-            Logout 
-          </div>
-        </Link>
-       
-      </>
-    )
+  if(isEmpty(profile)) return null
 
-    const guestLinks = (
-      <div>
-        <Link to="/login">Sign In</Link>
-        <Link to="/register">Sign Up</Link>
-      </div>
-    )
-
-
-    return (
-      <div>
-        <div className="dropdown" onClick={this.toggleClick}>
-          <div className='dropdown_hover'>
-            <div className="btn-line"></div>
-            <div className="btn-line"></div>
-            <div className="btn-line"></div>
-          </div>
-          { this.state.displayMenu ? (
-            <div className='dropdown_menu'>
-              { isAuthenticated ? authLinks : guestLinks }
-            </div>
-          ) : null }
+  const authLinks = (
+    <div>
+      {/* <Link to="/djs">DJs</Link> */}
+      <Link to={`/profile/${profile.handle}`}>Profile</Link>
+      <Link to="/feed">Feed</Link>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="#" onClick={onLogoutClick}>
+        <div style={{display: 'flex', alignItems: 'center'}}>
+          <img 
+            src={user.avatar}
+            alt={user.name} 
+            style={{ width: '25px', height: '22px', marginRight: '5px' }}
+          />
+          Logout 
         </div>
+      </Link>
+      
+    </div>
+  )
+
+  const guestLinks = (
+    <div>
+      <Link to="/login">Sign In</Link>
+      <Link to="/register">Sign Up</Link>
+    </div>
+  )
+
+  return (
+    <div ref={ref}>
+      <div className="dropdown" onClick={() => setDisplayMenu(prev => !prev)}>
+        <div className='dropdown_hover'>
+          <div className="btn-line"></div>
+          <div className="btn-line"></div>
+          <div className="btn-line"></div>
+        </div>
+        { displayMenu &&
+          <div className='dropdown_menu'>
+            { isAuthenticated ? authLinks : guestLinks }
+          </div>
+        }
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 DropdownMenu.propTypes = {
@@ -106,5 +104,8 @@ const mapStateToProps = state => ({
   auth: state.auth
 })
 
-
-export default connect(mapStateToProps, { logoutUser, clearCurrentProfile, getCurrentProfile })(withRouter(DropdownMenu))
+export default connect(mapStateToProps, { 
+  logoutUser, 
+  clearCurrentProfile, 
+  getCurrentProfile 
+})(withRouter(DropdownMenu))
