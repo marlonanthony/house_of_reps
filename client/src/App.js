db501dceca351fc18beeccf8c2c736c7aab32a5c
marlonanthony <mad1083@gmail.com>
2019-09-28 01:52:43 -0400

    moved redux and auth logic from App.js to index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,6 @@
 import React, { useState } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import jwt_decode from 'jwt-decode'
-import setAuthToken from './utils/setAuthToken'
 
-import { setCurrentUser, logoutUser } from './actions/authActions'
-import { clearCurrentProfile } from './actions/profileActions'
-import { Provider } from 'react-redux'
-import store from './store'
 import PrivateRoute from './components/common/PrivateRoute'
 import DropdownMenu from './components/common/dropdown_menu/DropdownMenu'
 import Landing from './pages/landing/Landing'
@@ -30,19 +24,6 @@ import FixedHighlights from './components/UI/uniterrupted_highlights/FixedHighli
 import Hashtag from './components/hashtags/Hashtag'
 import './App.css'
 
-// Check for token
-if(localStorage.jwtToken) {
-  setAuthToken(localStorage.jwtToken)                // Set auth token header auth
-  const decoded = jwt_decode(localStorage.jwtToken)  // Decode token and get user info and expiration
-  store.dispatch(setCurrentUser(decoded))            // Set user and isAuthenticated
-  const currentTime = Date.now() / 1000              // Check for expired token
-  if(decoded.exp < currentTime) {
-    store.dispatch(logoutUser())                     // Logout user 
-    store.dispatch(clearCurrentProfile())            // Clear current Profile
-    window.location.href = '/'                       // Redirect to homepage
-  }
-}
-
 const App = () => {
 
   const [showHighlight, setShowHighlight] = useState(false),
@@ -54,66 +35,64 @@ const App = () => {
   }
 
   return (
-    <Provider store={ store }>
-      <Router>
+    <Router>
+      <>
         <>
-          <>
-            <DropdownMenu />
-            <FixedHighlights 
-              showHighlight={showHighlight} 
-              toggleShowHighlight={toggleShowHighlight}
-              currentIndex={currentIndex}
-            />
-            <Route exact path='/' component={ Landing } />
-          </>
-          <div>
-            <Route exact path='/register' component={ Register } />
-            <Route exact path='/login' component={ Login } />
-            <Route exact path='/checkemail' component={ ConfirmEmail } />
-            <Route exact path='/verify' component={ Verify } />
-            <Switch>
-              <PrivateRoute exact path='/dashboard' component={ Dashboard } />
-            </Switch>
-            <Switch>
-              <PrivateRoute exact path='/create-profile' component={ CreateProfile } />
-            </Switch>
-            <Switch>
-              <PrivateRoute exact path='/edit-profile' component={ EditProfile } />
-            </Switch>
-            <Switch>
-              <PrivateRoute exact path='/add-venue' component={ AddVenue } />
-            </Switch>
-            <Switch>
-              <PrivateRoute exact path='/add-djpool' component={ AddDjpool } />
-            </Switch>
-            <Switch>
-              <PrivateRoute exact path='/add-store' component={ AddCertifiedStore } />
-            </Switch>
-            <Switch>
-              <PrivateRoute exact path='/add-perk' component={ AddPerk } />
-            </Switch>
-            <Switch>
-              <PrivateRoute exact path='/add-brand' component={ AddBrand } />
-            </Switch>
-            <Switch>
-              <PrivateRoute exact path='/feed' component={() => <Posts toggleShowHighlight={toggleShowHighlight} /> } />
-            </Switch>
-            <Switch>
-              <PrivateRoute exact path='/profile/:handle' component={ Profile } />
-            </Switch>
-            <Switch>
-              <PrivateRoute exact path='/djs' component={ Profiles } />
-            </Switch>
-            <Switch>
-              <PrivateRoute exact path='/notifications' component={ Notifications } />
-            </Switch>
-            <Switch>
-              <PrivateRoute exact path='/hashtag/:hashtag' component={ Hashtag } />
-            </Switch>
-          </div>
+          <DropdownMenu />
+          <FixedHighlights 
+            showHighlight={showHighlight} 
+            toggleShowHighlight={toggleShowHighlight}
+            currentIndex={currentIndex}
+          />
+          <Route exact path='/' component={ Landing } />
         </>
-      </Router>
-    </Provider>
+        <div>
+          <Route exact path='/register' component={ Register } />
+          <Route exact path='/login' component={ Login } />
+          <Route exact path='/checkemail' component={ ConfirmEmail } />
+          <Route exact path='/verify' component={ Verify } />
+          <Switch>
+            <PrivateRoute exact path='/dashboard' component={ Dashboard } />
+          </Switch>
+          <Switch>
+            <PrivateRoute exact path='/create-profile' component={ CreateProfile } />
+          </Switch>
+          <Switch>
+            <PrivateRoute exact path='/edit-profile' component={ EditProfile } />
+          </Switch>
+          <Switch>
+            <PrivateRoute exact path='/add-venue' component={ AddVenue } />
+          </Switch>
+          <Switch>
+            <PrivateRoute exact path='/add-djpool' component={ AddDjpool } />
+          </Switch>
+          <Switch>
+            <PrivateRoute exact path='/add-store' component={ AddCertifiedStore } />
+          </Switch>
+          <Switch>
+            <PrivateRoute exact path='/add-perk' component={ AddPerk } />
+          </Switch>
+          <Switch>
+            <PrivateRoute exact path='/add-brand' component={ AddBrand } />
+          </Switch>
+          <Switch>
+            <PrivateRoute exact path='/feed' component={() => <Posts toggleShowHighlight={toggleShowHighlight} /> } />
+          </Switch>
+          <Switch>
+            <PrivateRoute exact path='/profile/:handle' component={ Profile } />
+          </Switch>
+          <Switch>
+            <PrivateRoute exact path='/djs' component={ Profiles } />
+          </Switch>
+          <Switch>
+            <PrivateRoute exact path='/notifications' component={ Notifications } />
+          </Switch>
+          <Switch>
+            <PrivateRoute exact path='/hashtag/:hashtag' component={ Hashtag } />
+          </Switch>
+        </div>
+      </>
+    </Router>
   )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import jwt_decode from 'jwt-decode'
+
+import setAuthToken from './utils/setAuthToken'
+import { setCurrentUser, logoutUser } from './actions/authActions'
+import { clearCurrentProfile } from './actions/profileActions'
+import store from './store'
 import './index.css'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// Check for token
+if(localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken)                // Set auth token header auth
+  const decoded = jwt_decode(localStorage.jwtToken)  // Decode token and get user info and expiration
+  store.dispatch(setCurrentUser(decoded))            // Set user and isAuthenticated
+  const currentTime = Date.now() / 1000              // Check for expired token
+  if(decoded.exp < currentTime) {
+    store.dispatch(logoutUser())                     // Logout user 
+    store.dispatch(clearCurrentProfile())            // Clear current Profile
+    window.location.href = '/'                       // Redirect to homepage
+  }
+}
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <App />
+  </Provider>, 
+document.getElementById('root'))
