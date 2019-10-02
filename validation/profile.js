153218c79b70bde4a96ef3be1f35cc0a6392b121
marlonanthony <mad1083@gmail.com>
2019-10-01 20:26:52 -0400

    added "trim()" to backend profile validation such that "isURL" wont throw an error when a user leaves a space at the end

--- validation/profile.js
+++ validation/profile.js
@@ -9,47 +9,47 @@ module.exports = function validateProfileInput(data) {
   }
   
   if(!isEmpty(data.website)) {
-    if(!Validator.isURL(data.website)) {
+    if(!Validator.isURL(data.website.trim())) {
       errors.website = 'Not a valid URL'
     }
   }
   if(!isEmpty(data.twitter)) {
-    if(!Validator.isURL(data.twitter)) {
+    if(!Validator.isURL(data.twitter.trim())) {
       errors.twitter = 'Not a valid URL'
     }
   }
   if(!isEmpty(data.instagram)) {
-    if(!Validator.isURL(data.instagram)) {
+    if(!Validator.isURL(data.instagram.trim())) {
       errors.instagram = 'Not a valid URL'
     }
   }
   if(!isEmpty(data.facebook)) {
-    if(!Validator.isURL(data.facebook)) {
+    if(!Validator.isURL(data.facebook.trim())) {
       errors.facebook = 'Not a valid URL'
     }
   }
   if(!isEmpty(data.linkedin)) {
-    if(!Validator.isURL(data.linkedin)) {
+    if(!Validator.isURL(data.linkedin.trim())) {
       errors.linkedin = 'Not a valid URL'
     }
   }
   if(!isEmpty(data.soundcloud)) {
-    if(!Validator.isURL(data.soundcloud)) {
+    if(!Validator.isURL(data.soundcloud.trim())) {
       errors.soundcloud = 'Not a valid URL'
     }
   }
   if(!isEmpty(data.spotify)) {
-    if(!Validator.isURL(data.spotify)) {
+    if(!Validator.isURL(data.spotify.trim())) {
       errors.spotify = 'Not a valid URL'
     }
   }
   if(!isEmpty(data.mixcloud)) {
-    if(!Validator.isURL(data.mixcloud)) {
+    if(!Validator.isURL(data.mixcloud.trim())) {
       errors.mixcloud = 'Not a valid URL'
     }
   }
   if(!isEmpty(data.youtube)) {
-    if(!Validator.isURL(data.youtube)) {
+    if(!Validator.isURL(data.youtube.trim())) {
       errors.youtube = 'Not a valid URL'
     }
   }
