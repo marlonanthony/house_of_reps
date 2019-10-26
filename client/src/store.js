a14ebe7e03f7dc1620f1ba8524c372c439e3d95a
marlonanthony <mad1083@gmail.com>
2019-10-26 00:27:38 -0400

    refactored store.js

--- client/src/store.js
+++ client/src/store.js
@@ -2,17 +2,16 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
-const initialState = {}
-
-const middleware = [thunk]
-
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware)
-    // window.__REDUX_DEVTOOLS_EXTENSION__() && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = (state = {}) =>
+  createStore(
+    rootReducer,
+    state,
+    compose(
+      applyMiddleware(thunk),
+      process.env.NODE_ENV === 'development' &&
+        window.__REDUX_DEVTOOLS_EXTENSION__() &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
   )
-)
 
 export default store
