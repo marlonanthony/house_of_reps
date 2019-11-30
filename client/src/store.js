a278ea1286e4514a0774622e5152f7f3a45e22bc
marlonanthony <mad1083@gmail.com>
2019-11-30 16:01:28 -0500

    prod

--- client/src/store.js
+++ client/src/store.js
@@ -6,12 +6,12 @@ const initialState = {}
 
 const store = createStore(
   rootReducer,
-  initialState
-  // compose(
-  //   applyMiddleware(thunk),
-  //   window.__REDUX_DEVTOOLS_EXTENSION__() &&
-  //     window.__REDUX_DEVTOOLS_EXTENSION__()
-  // )
+  initialState,
+  compose(
+    applyMiddleware(thunk)
+    // window.__REDUX_DEVTOOLS_EXTENSION__() &&
+    // window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 )
 
 export default store
