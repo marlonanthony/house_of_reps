59b578c6002b1d246c2ba402eed8c4cd3e11226e
marlonanthony <mad1083@gmail.com>
2020-02-07 18:56:10 -0500

    refactored PoolsContainer.js

--- client/src/components/posts/post-assets/promos/djpools/PoolsContainer.js
+++ client/src/components/posts/post-assets/promos/djpools/PoolsContainer.js
@@ -2,19 +2,16 @@ import React from 'react'
 import DjPools from './DjPools'
 
 export default function PoolsContainer({ profiles }) {
-  let djpools
+  if (!profiles) return null
 
-  if (!profiles) {
-    djpools = null
-  } else {
-    djpools = profiles.map(val =>
-      val.djpools.length > 0 && val.djpools !== null
-        ? val.djpools.map(djpool => (
-            <DjPools key={djpool._id} djpools={val.djpools} djpool={djpool} />
-          ))
-        : null
-    )
-  }
-
-  return <div className="djpools">{djpools}</div>
+  return profiles.map(
+    val =>
+      val.djpools &&
+      val.djpools.length &&
+      val.djpools.map(djpool => (
+        <div className="djpools">
+          <DjPools key={djpool._id} djpools={val.djpools} djpool={djpool} />
+        </div>
+      ))
+  )
 }
