64baa71fe1c203a4f94344a2cf81e4df16cbe7b3
marlonanthony <mad1083@gmail.com>
2020-02-10 17:33:55 -0500

    refactored NestedCommentBody into a functional component

--- client/src/components/posts/nested_comments/nested_comment_assets/nested_comment_body/NestedCommentBody.js
+++ client/src/components/posts/nested_comments/nested_comment_assets/nested_comment_body/NestedCommentBody.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import JSEMOJI from 'emoji-js'
 
 import { editNestedCommentAction } from '../../../../../actions/postActions'
 import { youTubeURL } from '../../../../../utils/youTubeUrl'
@@ -9,83 +8,32 @@ import DefaultNestedCommentBody from './default_nested_comment_body/DefaultNeste
 import EditNestedComment from './edit_nested_comment/EditNestedComment'
 import './NestedCommentBody.css'
 
-class NestedCommentBody extends Component {
-  state = {
-    text: '',
-    showEmojis: false,
-    errors: {}
-  }
+function NestedCommentBody({
+  comment,
+  nestedComment,
+  postId,
+  editPost,
+  toggleEditPost,
+  editNestedCommentAction
+}) {
+  let youtubeUrl = nestedComment.url
+  youtubeUrl = youTubeURL(youtubeUrl)
 
-  onChange = e => this.setState({ [e.target.name]: e.target.value })
-
-  toggleEmoji = () => {
-    this.setState(prevState => ({ showEmojis: !prevState.showEmojis }))
-  }
-
-  addEmoji = emojiName => {
-    const jsemoji = new JSEMOJI()
-    jsemoji.img_set = 'emojione'
-    jsemoji.img_sets.emojione.path =
-      'https://cdn.jsdelivr.net/emojione/assets/3.0/png/32/'
-    jsemoji.supports_css = false
-    jsemoji.allow_native = false
-    jsemoji.replace_mode = 'unified'
-    jsemoji.text_mode = true
-    jsemoji.include_title = true
-    jsemoji.replace_unified(`:${emojiName}:`)
-    jsemoji.replace_colons(`:${emojiName}:`)
-
-    let emoji = String.fromCodePoint(parseInt(emojiName, 16))
-    this.setState({ text: this.state.text + emoji })
-  }
-
-  editNestedComment = e => {
-    e.preventDefault()
-    const { text } = this.state
-    const {
-      comment: { _id },
-      nestedComment,
-      postId,
-      toggleEditPost
-    } = this.props
-    const editedNestedComment = { text }
-    this.props.editNestedCommentAction(
-      postId,
-      _id,
-      nestedComment._id,
-      editedNestedComment
-    )
-    toggleEditPost()
-    this.setState({ text: '' })
-    e.target.reset()
-  }
-
-  render() {
-    const { nestedComment, editPost } = this.props,
-      { text, showEmojis, errors } = this.state
-
-    let youtubeUrl = nestedComment.url
-    youtubeUrl = youTubeURL(youtubeUrl)
-
-    return !editPost ? (
-      <DefaultNestedCommentBody
-        nestedComment={nestedComment}
-        youtubeUrl={youtubeUrl}
-      />
-    ) : (
-      <EditNestedComment
-        showEmojis={showEmojis}
-        nestedComment={nestedComment}
-        editNestedComment={this.editNestedComment}
-        text={text}
-        errors={errors}
-        addEmoji={this.addEmoji}
-        onChange={this.onChange}
-        toggleEmoji={this.toggleEmoji}
-        youtubeUrl={youtubeUrl}
-      />
-    )
-  }
+  return !editPost ? (
+    <DefaultNestedCommentBody
+      nestedComment={nestedComment}
+      youtubeUrl={youtubeUrl}
+    />
+  ) : (
+    <EditNestedComment
+      nestedComment={nestedComment}
+      youtubeUrl={youtubeUrl}
+      comment={comment}
+      postId={postId}
+      toggleEditPost={toggleEditPost}
+      editNestedCommentAction={editNestedCommentAction}
+    />
+  )
 }
 
 NestedCommentBody.propTypes = {
@@ -93,7 +41,8 @@ NestedCommentBody.propTypes = {
   nestedComment: PropTypes.object.isRequired,
   postId: PropTypes.string.isRequired,
   editPost: PropTypes.bool.isRequired,
-  toggleEditPost: PropTypes.func.isRequired
+  toggleEditPost: PropTypes.func.isRequired,
+  editNestedCommentAction: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
--- client/src/components/posts/nested_comments/nested_comment_assets/nested_comment_body/edit_nested_comment/EditNestedComment.js
+++ client/src/components/posts/nested_comments/nested_comment_assets/nested_comment_body/edit_nested_comment/EditNestedComment.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import EmojiPicker from 'emoji-picker-react'
+import JSEMOJI from 'emoji-js'
 
 import LightBackdrop from '../../../../../UI/backdrop/LightBackdrop'
 import Icon from '../../../../../UI/icons/Icon'
@@ -7,19 +8,53 @@ import EmojiModal from '../../../../../UI/modal/EmojiModal'
 import TextAreaForm from '../../../../../common/textarea/TextAreaForm'
 
 export default function EditNestedComment({
-  showEmojis,
   nestedComment,
-  text,
-  errors,
-  editNestedComment,
-  addEmoji,
-  onChange,
-  toggleEmoji,
+  toggleEditPost,
+  editNestedCommentAction,
+  comment,
+  postId,
   youtubeUrl
 }) {
+  const [errors, setErrors] = useState({}),
+    [text, setText] = useState(nestedComment.text || ''),
+    [showEmojis, setShowEmojis] = useState(false)
+
+  const toggleEmoji = () => {
+    setShowEmojis(prev => !prev)
+  }
+
+  const onChange = e => setText(e.target.value)
+
+  const addEmoji = emojiName => {
+    const jsemoji = new JSEMOJI()
+    jsemoji.img_set = 'emojione'
+    jsemoji.img_sets.emojione.path =
+      'https://cdn.jsdelivr.net/emojione/assets/3.0/png/32/'
+    jsemoji.supports_css = false
+    jsemoji.allow_native = false
+    jsemoji.replace_mode = 'unified'
+    jsemoji.text_mode = true
+    jsemoji.include_title = true
+    jsemoji.replace_unified(`:${emojiName}:`)
+    jsemoji.replace_colons(`:${emojiName}:`)
+
+    let emoji = String.fromCodePoint(parseInt(emojiName, 16))
+    setText(prevText => prevText + emoji)
+  }
+
+  const editNestedComment = e => {
+    e.preventDefault()
+    const { _id } = comment
+    const editedNestedComment = { text }
+    editNestedCommentAction(postId, _id, nestedComment._id, editedNestedComment)
+    toggleEditPost()
+    setText('')
+    e.target.reset()
+  }
+
   return (
     <>
-      <LightBackdrop clicked={this.toggleEmoji} show={showEmojis} />
+      <LightBackdrop clicked={toggleEmoji} show={showEmojis} />
       {showEmojis && (
         <EmojiModal>
           <EmojiPicker onEmojiClick={addEmoji} />
