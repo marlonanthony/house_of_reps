d7779929cfca222270dba069150d8b39c500f331
marlonanthony <mad1083@gmail.com>
2020-02-10 19:44:14 -0500

    converted CommentBody.js into Hooks

--- client/src/components/posts/comments/comment_assets/comment_body/CommentBody.js
+++ client/src/components/posts/comments/comment_assets/comment_body/CommentBody.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import JSEMOJI from 'emoji-js'
 
@@ -8,28 +8,34 @@ import DefaultCommentBody from './comment_body_assets/default_comment_body/Defau
 import EditComment from './comment_body_assets/edit_comment/EditComment'
 import './CommentBody.css'
 
-class CommentBody extends Component {
-  state = { text: this.props.comment.text || '', showEmojis: false }
+function CommentBody({
+  comment,
+  modalShow,
+  editPost,
+  toggleEditPost,
+  postId,
+  editedCommentAction
+}) {
+  const [text, setText] = useState(comment.text || ''),
+    [showEmojis, setShowEmojis] = useState(false)
 
-  onChange = e => this.setState({ [e.target.name]: e.target.value })
+  const onChange = e => setText(e.target.value)
 
-  onSubmit = e => {
+  const onSubmit = e => {
     e.preventDefault()
-    const { text } = this.state
-    const { _id } = this.props.comment
-    const { toggleEditPost, postId } = this.props
+    const { _id } = comment
     const editedComment = { text }
-    this.props.editedCommentAction(postId, _id, editedComment)
+    editedCommentAction(postId, _id, editedComment)
     toggleEditPost()
-    this.setState({ text: '' })
+    setText('')
     e.target.reset()
   }
 
-  toggleEmoji = () => {
-    this.setState(prevState => ({ showEmojis: !prevState.showEmojis }))
+  const toggleEmoji = () => {
+    setShowEmojis(prev => !prev)
   }
 
-  addEmoji = emojiName => {
+  const addEmoji = emojiName => {
     const jsemoji = new JSEMOJI()
     jsemoji.img_set = 'emojione'
     jsemoji.img_sets.emojione.path =
@@ -43,39 +49,34 @@ class CommentBody extends Component {
     jsemoji.replace_colons(`:${emojiName}:`)
 
     let emoji = String.fromCodePoint(parseInt(emojiName, 16))
-    this.setState({ text: this.state.text + emoji })
+    setText(prevText => prevText + emoji)
   }
 
-  render() {
-    const { text, showEmojis } = this.state
-    const { comment, modalShow, editPost } = this.props
+  let youtubeUrl = comment.url
+  youtubeUrl = youTubeURL(youtubeUrl)
 
-    let youtubeUrl = comment.url
-    youtubeUrl = youTubeURL(youtubeUrl)
-
-    if (!editPost) {
-      return (
-        <DefaultCommentBody
-          comment={comment}
-          modalShow={modalShow}
-          youtubeUrl={youtubeUrl}
-        />
-      )
-    } else {
-      return (
-        <EditComment
-          showEmojis={showEmojis}
-          comment={comment}
-          text={text}
-          modalShow={modalShow}
-          youtubeUrl={youtubeUrl}
-          toggleEmoji={this.toggleEmoji}
-          addEmoji={this.addEmoji}
-          onSubmit={this.onSubmit}
-          onChange={this.onChange}
-        />
-      )
-    }
+  if (!editPost) {
+    return (
+      <DefaultCommentBody
+        comment={comment}
+        modalShow={modalShow}
+        youtubeUrl={youtubeUrl}
+      />
+    )
+  } else {
+    return (
+      <EditComment
+        showEmojis={showEmojis}
+        comment={comment}
+        text={text}
+        modalShow={modalShow}
+        youtubeUrl={youtubeUrl}
+        toggleEmoji={toggleEmoji}
+        addEmoji={addEmoji}
+        onSubmit={onSubmit}
+        onChange={onChange}
+      />
+    )
   }
 }
 
