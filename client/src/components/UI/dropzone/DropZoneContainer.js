97de78d917a4bae86dc4542ac540ca7a26abc7b9
marlonanthony <mad1083@gmail.com>
2020-02-11 22:14:30 -0500

    refactored DropZoneContainer.js

--- client/src/components/UI/dropzone/DropZoneContainer.js
+++ client/src/components/UI/dropzone/DropZoneContainer.js
@@ -8,25 +8,21 @@ export default function DropZoneContainer({
 }) {
   return (
     <div className="dropzone-container">
-      <div className="FileUpload">
-        <Dropzone
-          className="dropzone"
-          multiple={false}
-          accept="image/*"
-          onDrop={onImageDrop}
-        >
-          <p>Drag or click here to upload a file.</p>
-        </Dropzone>
-      </div>
-      <div>
-        {uploadedFileCloudinaryUrl === '' ? null : (
-          <img
-            src={uploadedFileCloudinaryUrl}
-            className="dropzone_img"
-            alt={uploadedFile.name}
-          />
-        )}
-      </div>
+      <Dropzone
+        className="dropzone"
+        multiple={false}
+        accept="image/*"
+        onDrop={onImageDrop}
+      >
+        <p>Drag or click here to upload a file.</p>
+      </Dropzone>
+      {uploadedFileCloudinaryUrl && (
+        <img
+          src={uploadedFileCloudinaryUrl}
+          className="dropzone_img"
+          alt={uploadedFile.name}
+        />
+      )}
     </div>
   )
 }
