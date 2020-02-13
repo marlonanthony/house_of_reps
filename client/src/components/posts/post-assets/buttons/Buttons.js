30b9c9016cdef83b78d5e701339d0e292eb92cbc
marlonanthony <mad1083@gmail.com>
2020-02-12 23:54:21 -0500

    redid post responsiveness

--- client/src/components/posts/post-assets/buttons/Buttons.js
+++ client/src/components/posts/post-assets/buttons/Buttons.js
@@ -9,6 +9,7 @@ export default function Buttons({ showPostByHashtag, hashtag, showHashtags }) {
           src={require('../../../../img/post_buttons/PNG_SMALL/sale_ON.png')}
           alt="marketplace"
           title="marketplace"
+          className="reps-btn-imgs"
         />
       ) : (
         <img
@@ -16,6 +17,7 @@ export default function Buttons({ showPostByHashtag, hashtag, showHashtags }) {
           src={require('../../../../img/post_buttons/PNG_SMALL/SALE_OFF.png')}
           alt="marketplace"
           title="marketplace"
+          className="reps-btn-imgs"
         />
       )}
       {hashtag === 'mixes' && showHashtags ? (
@@ -24,6 +26,7 @@ export default function Buttons({ showPostByHashtag, hashtag, showHashtags }) {
           src={require('../../../../img/post_buttons/PNG_SMALL/MIXES_ON_WR.png')}
           alt="mixes"
           title="mixes"
+          className="reps-btn-imgs"
         />
       ) : (
         <img
@@ -31,6 +34,7 @@ export default function Buttons({ showPostByHashtag, hashtag, showHashtags }) {
           src={require('../../../../img/post_buttons/PNG_SMALL/MIXES_OFF_WR.png')}
           alt="mixes"
           title="mixes"
+          className="reps-btn-imgs"
         />
       )}
       {hashtag === 'video' && showHashtags ? (
@@ -39,6 +43,7 @@ export default function Buttons({ showPostByHashtag, hashtag, showHashtags }) {
           src={require('../../../../img/post_buttons/PNG_SMALL/PLAY_ON_WR.png')}
           alt="video"
           title="video"
+          className="reps-btn-imgs"
         />
       ) : (
         <img
@@ -46,6 +51,7 @@ export default function Buttons({ showPostByHashtag, hashtag, showHashtags }) {
           src={require('../../../../img/post_buttons/PNG_SMALL/PLAY_OFF_WR.png')}
           alt="video"
           title="video"
+          className="reps-btn-imgs"
         />
       )}
       {hashtag === 'downloads' && showHashtags ? (
@@ -54,6 +60,7 @@ export default function Buttons({ showPostByHashtag, hashtag, showHashtags }) {
           src={require('../../../../img/post_buttons/PNG_SMALL/DOWNLOAD_ON_WR.png')}
           alt="downloads"
           title="downloads"
+          className="reps-btn-imgs"
         />
       ) : (
         <img
@@ -61,6 +68,7 @@ export default function Buttons({ showPostByHashtag, hashtag, showHashtags }) {
           src={require('../../../../img/post_buttons/PNG_SMALL/DOWNLOAD_OFF_WR.png')}
           alt="downloads"
           title="downloads"
+          className="reps-btn-imgs"
         />
       )}
       {hashtag === 'news' && showHashtags ? (
@@ -69,6 +77,7 @@ export default function Buttons({ showPostByHashtag, hashtag, showHashtags }) {
           src={require('../../../../img/post_buttons/PNG_SMALL/ANOUCEMENTS_ON_WR.png')}
           alt="news"
           title="news"
+          className="reps-btn-imgs"
         />
       ) : (
         <img
@@ -76,6 +85,7 @@ export default function Buttons({ showPostByHashtag, hashtag, showHashtags }) {
           src={require('../../../../img/post_buttons/PNG_SMALL/ANOUCEMENTS_OFF.png')}
           alt="news"
           title="news"
+          className="reps-btn-imgs"
         />
       )}
       {hashtag === 'gigswap' && showHashtags ? (
@@ -84,6 +94,7 @@ export default function Buttons({ showPostByHashtag, hashtag, showHashtags }) {
           src={require('../../../../img/post_buttons/PNG_SMALL/GIG_SWAP_ON_WR.png')}
           alt="gigswap"
           title="gigswap"
+          className="reps-btn-imgs"
         />
       ) : (
         <img
@@ -91,6 +102,7 @@ export default function Buttons({ showPostByHashtag, hashtag, showHashtags }) {
           src={require('../../../../img/post_buttons/PNG_SMALL/GIG_SWAP_OFF_WR.png')}
           alt="gigswap"
           title="gigswap"
+          className="reps-btn-imgs"
         />
       )}
       {hashtag === 'help' && showHashtags ? (
@@ -99,6 +111,7 @@ export default function Buttons({ showPostByHashtag, hashtag, showHashtags }) {
           src={require('../../../../img/post_buttons/PNG_SMALL/HELP_ON_WR.png')}
           alt="help"
           title="help"
+          className="reps-btn-imgs"
         />
       ) : (
         <img
@@ -106,6 +119,7 @@ export default function Buttons({ showPostByHashtag, hashtag, showHashtags }) {
           src={require('../../../../img/post_buttons/PNG_SMALL/HELP_OFF_WR.png')}
           alt="help"
           title="help"
+          className="reps-btn-imgs"
         />
       )}
       {hashtag === 'recommendations' && showHashtags ? (
@@ -114,6 +128,7 @@ export default function Buttons({ showPostByHashtag, hashtag, showHashtags }) {
           src={require('../../../../img/post_buttons/PNG_SMALL/RECOMENDATIONS_ON.png')}
           alt="recommendations"
           title="recommendations"
+          className="reps-btn-imgs"
         />
       ) : (
         <img
@@ -121,6 +136,7 @@ export default function Buttons({ showPostByHashtag, hashtag, showHashtags }) {
           src={require('../../../../img/post_buttons/PNG_SMALL/RECOMENDATIONS_OFF_WR.png')}
           alt="recommendations"
           title="recommendations"
+          className="reps-btn-imgs"
         />
       )}
       {hashtag === 'conversation' && showHashtags ? (
@@ -129,6 +145,7 @@ export default function Buttons({ showPostByHashtag, hashtag, showHashtags }) {
           src={require('../../../../img/post_buttons/PNG_SMALL/CONVERATION_ON_WR.png')}
           alt="conversation"
           title="conversation"
+          className="reps-btn-imgs"
         />
       ) : (
         <img
@@ -136,6 +153,7 @@ export default function Buttons({ showPostByHashtag, hashtag, showHashtags }) {
           src={require('../../../../img/post_buttons/PNG_SMALL/CONVERATION_OFF_WR.png')}
           alt="conversation"
           title="conversation"
+          className="reps-btn-imgs"
         />
       )}
     </div>
