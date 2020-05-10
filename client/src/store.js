4990c6303476395a5de13540d0c47f67f770be09
marlonanthony <mad1083@gmail.com>
2020-05-09 21:16:43 -0400

    created addPromo action

--- client/src/store.js
+++ client/src/store.js
@@ -8,9 +8,9 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(thunk)
-    // window.__REDUX_DEVTOOLS_EXTENSION__() &&
-    //   window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__() &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 )
 
--- routes/api/promos.js
+++ routes/api/promos.js
@@ -38,7 +38,9 @@ router.delete(
   passport.authenticate('jwt', { session: false }),
   async (req, res) => {
     try {
-      if (!req.user.isAdmin) return res.status(401).json({unauthorized: 'Unauthorized!'})
+      if (!req.user.isAdmin) {
+        return res.status(401).json({unauthorized: 'Unauthorized!'})
+      }
       const promo = await Promo.findById(req.params.id)
       promo.remove()
       return res.status(200).json({ success: true })
